e07f848fa8047e94bef75172aae4c4afe9c1d9fe
Luis Daniel Alducin Acevedo <luisalduucin@users.noreply.github.com>
2017-02-13 10:52:42 -0600

    Added top level model

---
+++ app/src/main/java/com/nearsoft/flights/flightchecker/models/APIResponse.java
@@ -0,0 +1,19 @@
+package com.nearsoft.flights.flightchecker.models;
+
+/**
+ * Created by luisdanielalducinacevedo on 2/13/17.
+ */
+
+public class APIResponse {
+
+    Itinerary itinerary;
+
+    public Itinerary getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(Itinerary itinerary) {
+        this.itinerary = itinerary;
+    }
+
+}
