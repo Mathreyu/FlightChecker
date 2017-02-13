6e9c9ad9b29fb4cb56c3d2f648e77fd522406780
Luis Daniel Alducin Acevedo <luisalduucin@users.noreply.github.com>
2017-02-13 11:22:21 -0600

    Modified FlightAPI

--- app/src/main/java/com/nearsoft/flights/flightchecker/api/FlightApi.java
+++ app/src/main/java/com/nearsoft/flights/flightchecker/api/FlightApi.java
@@ -1,17 +1,17 @@
 package com.nearsoft.flights.flightchecker.api;
 
 
-import com.nearsoft.flights.flightchecker.models.Itinerary;
-
-import java.util.List;
+import com.nearsoft.flights.flightchecker.models.APIResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface FlightApi {
 
     String ENDPOINT = "";
 
-    @GET("")
-    Call<List<Itinerary>> getIntineryList();
+    @GET("itinerary/{id}")
+    Call<APIResponse> getIntinerary(@Path("id") int id);
+
 }
