e2d88992d9a5a741e19e268a04552142ef348db2
Mathreyu <Midna1213!>
2017-02-16 10:49:46 -0700

    Added DI and changed a few things on views

--- app/src/main/java/com/nearsoft/flights/flightchecker/QueryFlights.java
+++ app/src/main/java/com/nearsoft/flights/flightchecker/QueryFlights.java
@@ -2,31 +2,27 @@ package com.nearsoft.flights.flightchecker;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nearsoft.flights.flightchecker.api.FlightApi;
-import com.nearsoft.flights.flightchecker.models.APIResponse;
-import com.nearsoft.flights.flightchecker.models.FlightSegment;
+import com.nearsoft.flights.flightchecker.api.dagger.DaggerDIComponents;
+import com.nearsoft.flights.flightchecker.api.dagger.RetrofitModule;
+import com.nearsoft.flights.flightchecker.presenter.QueryFlightsPresenter;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class QueryFlights extends AppCompatActivity {
 
@@ -42,6 +38,12 @@ public class QueryFlights extends AppCompatActivity {
     @BindView(R.id.departureAirportInput)
     AutoCompleteTextView departureAirportInput;
 
+    @BindView(R.id.search)
+    Button searchButton;
+
+    @Inject
+    QueryFlightsPresenter queryFlightsPresenter;
+
     private Calendar calendar;
     private final SimpleDateFormat simpleDateformat = new SimpleDateFormat("dd - MMM - yyyy", Locale.US);
 
@@ -51,11 +53,17 @@ public class QueryFlights extends AppCompatActivity {
         setContentView(R.layout.activity_query_flights);
 
         ButterKnife.bind(QueryFlights.this);
+        DaggerDIComponents.builder().retrofitModule(new RetrofitModule(FlightApi.BASE_URL)).build().inject(this);
+
         calendar = Calendar.getInstance();
 
         String todayDate = simpleDateformat.format(calendar.getTime());
         departureQueryTime.setText(todayDate);
         arrivalQueryTime.setText(todayDate);
+
+        searchButton.setOnClickListener((View view) -> {
+           searchFlights(view);
+        });
     }
 
     protected void openCalendar(View view) {
@@ -66,7 +74,7 @@ public class QueryFlights extends AppCompatActivity {
             calendar.set(Calendar.MONTH, monthOfYear);
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
-            switch(view.getId()) {
+            switch (view.getId()) {
                 case R.id.departureQueryTime:
                     departureQueryTime.setText(simpleDateformat.format(calendar.getTime()));
                     break;
@@ -82,39 +90,21 @@ public class QueryFlights extends AppCompatActivity {
                 calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
-    public Retrofit provideRetrofit() {
-        return new Retrofit.Builder()
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(FlightApi.BASE_URL)
-                .build();
-    }
-
     protected void searchFlights(View view) {
 
-        final int toastLenght = Toast.LENGTH_SHORT;
+        final int toastLength = Toast.LENGTH_SHORT;
 
-        if ("".equals(departureAirportInput.getText().toString())) {
-            Toast.makeText(this, "Select a departure airport", toastLenght).show();
+        String departureAirport = departureAirportInput.getText().toString();
+        String arrivalAirport = arrivalAirportInput.getText().toString();
+
+        if ("".equals(departureAirport)) {
+            Toast.makeText(this, "Select a departure airport", toastLength).show();
             return;
-        } else if ("".equals(arrivalAirportInput.getText().toString())) {
-            Toast.makeText(this, "Select an arrival airport", toastLenght).show();
+        } else if ("".equals(arrivalAirport)) {
+            Toast.makeText(this, "Select an arrival airport", toastLength).show();
             return;
         }
-
-        final Retrofit retrofit = provideRetrofit();
-        final FlightApi flightApi = retrofit.create(FlightApi.class);
-
-        Observable<APIResponse> apiService = flightApi.searchFlights(
-                departureAirportInput.getText().toString(),
-                arrivalAirportInput.getText().toString());
-
-        apiService.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(flight -> {
-                    List<FlightSegment> responses = flight.getItinerary().getOriginDestinationOptions().get(0).getFlightSegments();
-                    //adapter.addResults(responses);
-                }, Throwable::printStackTrace);
+        queryFlightsPresenter.searchRetrofit(departureAirport, arrivalAirport);
 
         Intent intent = new Intent(this, FlightsMain.class);
         startActivity(intent);
