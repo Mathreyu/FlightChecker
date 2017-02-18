e3250cc1222f2a91023c9b879b507e4d1cfcd9c0
Mathreyu <Midna1213!>
2017-02-18 14:17:06 -0700

    UI modifications.
    Added new colors and changer search view to be more aligned and inside a CardView.

--- app/src/main/java/com/nearsoft/flights/flightchecker/api/FlightsService.java
+++ app/src/main/java/com/nearsoft/flights/flightchecker/api/FlightsService.java
@@ -16,9 +16,6 @@ public interface FlightsService {
     //String BASE_URL = "http://www.mocky.io/v2/58a1f958130000740a4e070d/";
     String BASE_URL = "http://192.168.136.201:8080/";
 
-    @GET("itinerary/")
-    Call<List<APIResponse>> getItineraries();
-
     @GET("itinerary/{id}")
     Observable<APIResponse> getItinerary(@Path("id") int id);
 
